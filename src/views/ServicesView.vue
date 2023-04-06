3a8a29b41cd9fd5823d7f2c42102bb7f4eacd7d9
QL-skennedy <skennedy@qualitylogic.com>
2023-04-06 16:47:13 -0600

    working on getting carousel working

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,6 +1,32 @@
 <template>
     <div class='services'>
-        this is the services page
+        <div class="row">
+            <div class="col">
+                <div id="carouselExample" class="carousel slide">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../assets/TroyExchange.png" class="" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../assets/GemOnly.png" class="" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../assets/trackHeaderImage.png" class="" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
