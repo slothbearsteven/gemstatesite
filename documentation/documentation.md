9368ac2564ec86478512d5f3a910c4e4e93b36a5
Beugenius <Beugenius@users.noreply.github.com>
2025-01-25 02:49:13 +0000

    deploy: 3da8d1d045693e1b6e85d62d7763a616cd55e247

---
+++ documentation/documentation.md
@@ -0,0 +1,39 @@
+# Project Documentation
+
+## Introduction
+This document provides detailed information about the specific implementations and utilities included in the project.
+
+## Project Layout
+The project uses Bootstrap for the main page layout to ensure responsiveness and mobile compatibility. Custom CSS styles are applied to enhance Bootstrap's default capabilities, addressing specific needs like background images, color adjustments tailored for different screen resolutions, font sizes, and general spacing adjustments.
+
+### Directory Structure
+- `src/assets` - Contains all static resources
+- `src/assets/resized` - Contains all resized images. Place here after running the dev-tools/resize-images.ps1 (instructions below).
+- `public/styling.css` - CSS File containing all stylings outside of Bootstrap
+- `src/router/index.js` - Router used for site navigation. 
+- `src/views` - Page templates/.vue files. One for each page needed. 
+- `/dev-tools` - Includes utilities for developers such as the image resizing tool.
+
+## Styles and Themes
+Custom CSS is used for detailed styling beyond what Bootstrap provides. This includes:
+- Background images setup for various sections.
+- Color schemes tailored to enhance visual aesthetics.
+- Responsive designs specifically fine-tuned for multiple screen resolutions.
+
+### Responsive Design
+Media queries are extensively used to ensure that the website functions well on devices with various screen sizes, from mobile phones to large desktop monitors.
+
+## Image Resizing Tool
+An automated tool for resizing images is included under `dev-tools/`. This tool utilizes `cwebp` to efficiently convert images into the WebP format, optimizing them for web use while maintaining high-quality standards.
+
+### Image Resizing Tool Usage
+To use the image resizing tool, follow these steps:
+1. Place the images that need to be resized in the `dev-tools/resources` folder.
+2. Right-click on `resize-images.ps1` and select "Run with PowerShell."
+3. The script uses `cwebp` to convert and resize the images, which will then be available in `dev-tools/reformatted` once the script completes its execution.
+4. Move the images from `dev-tools/reformatted` to `src/assets/resized`
+
+## Adding Images to the Carousel on the Services Page 
+1. Copy and paste this inside of the div with class 'carousel-inner': `<div class="carousel-item"><img src="../assets/resized/<name of image>.webp" class="" alt="description of image"></div>`
+2. Replace `<name of image>` from that line with the name of the newly resized image
+3. Re-run the site and it will be there 
---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="main-container">
+    <div id="mainHeaderDiv">
+
+      <div id="mainNavDiv" class="fixed-top">
+        <nav id="mainNav" class="navbar navbar-expand-sm navbar-toggleable-sm p-0">
+          <div id="logoDiv" class="">
+            <a href="#/">
+              <h1 id="logoText" class="text-white">Gem State Tracks</h1>
+              <img id="logoGem" src="./assets/resized/GemOnly.png" />
+            </a>
+          </div>
+          <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse"
+            data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon navbar-light"></span>
+          </button>
+          <div id="navListDiv" class="navbar-collapse collapse d-sm-inline-flex">
+            <ul id="navList" class="navbar-nav flex-grow-1 justify-content-end">
+              <li class="nav-item me-3" @click="closeNavBar">
+                <router-link class="nav-router" to="/">Home</router-link>
+              </li>
+              <li class="nav-item me-3" @click="closeNavBar">
+                <router-link class="nav-router" to="/services">Services</router-link>
+              </li>
+              <li class="nav-item me-3" @click="closeNavBar">
+                <router-link class="nav-router" to="/contactUs">Contact Us</router-link>
+              </li>
+
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <router-view class="bod" />
+    <div class="row footer justify-content-center mainFooterDiv">
+
+      <div id="footerDiv" class="">
+        <div id="footerLogo" class="col-md-6">
+          <h1 id="footerLogoText" class="text-white">Gem State Tracks</h1>
+          <img id="footerLogoGem" src="./assets/resized/GemOnly.png" />
+        </div>
+        <div class="footerContact">
+          <div class="col-md-6">
+            <a href="tel:+12087894544" class="noTextDecorationWhite">+1 208-789-4544</a>
+          </div>
+          <div class="col-md-6">
+            <a href="mailto:neil@gemstatetracks.com" class="noTextDecorationWhite">neil@gemstatetracks.com</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+}
+
+nav {
+  padding: 5px;
+}
+
+nav a {
+  font-weight: normal;
+  color: #ffffff;
+}
+
+nav a.router-link-exact-active {
+  color: #1d7cbc;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    closeNavBar() {
+      document.getElementsByClassName("navbar-toggler")[0].click();
+    }
+  }
+}
+</script>
